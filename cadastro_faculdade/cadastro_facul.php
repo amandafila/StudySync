2070642dec8702d518143d391d58a65f73ae51a0
Carloslgp <carloslgp3585@gmail.com>
2025-03-28 10:53:12 -0300

    update cadastro_faculdade

--- cadastro_faculdade/cadastro_facul.php
+++ cadastro_faculdade/cadastro_facul.php
@@ -6,51 +6,47 @@
   <title>Cadastro de Faculdade</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link href="../assets/styles/cadastro_facul.css" rel="stylesheet">
 </head>
 
 <body>
-  <div class="mt-2 container-fluid">
-    <div class="row">
-      <div class="col-1">
-        <i class="fa fa-check" style="font-size:30px;"></i>
-      </div>
-      <div class="col-2">
-        <h5>Cadastro de Faculdade</h5>
-      </div>
+  <div class="left">
+  <div class="div_left_title">
+      <h5 class="title_left">StudySync</h5>
     </div>
-    <div class="row">
-      <div class="col-6">
-        <form method="POST" action="cadastro_facul.php" enctype="multipart/form-data">
-          <div class="mb-2">
-            <input type="text" name="nome" class="form-control" placeholder="Nome da faculdade" required>
-          </div>
-          <div class="mb-2">
-            <input type="text" name="cnpj" class="form-control" placeholder="CNPJ" required>
-          </div>
-          <div class="mb-2">
-            <input type="text" name="cep" class="form-control" placeholder="CEP" required>
-          </div>
-          <div class="mb-2">
-            <input type="text" name="telefone" class="form-control" placeholder="Telefone" required>
-          </div>
-          <div class="mb-2">
-            <input type="text" name="usuario" class="form-control" placeholder="Usuário" required>
-          </div>
-          <div class="mb-2">
-            <input type="email" name="email" class="form-control" placeholder="E-mail" required>
-          </div>
-          <div class="mb-2">
-            <input type="password" name="senha" class="form-control" placeholder="Senha" required>
-          </div>
-          <div class="mb-2">
-            <input type="file" name="documentacao" class="form-control" placeholder="Documentação">
-          </div>
-          <button type="submit" class="mt-2 btn btn-primary">Cadastrar</button>
-        </form>
-      </div>
+    <div class="div_left_paragraph">
+      <p class="left_paragraph">Faça parte dessa comunidade incrível. </p>
+    </div>
+  </div>
+  <div class="right">
+    <div class="col-6">
+      <form method="POST" action="cadastro_facul.php" enctype="multipart/form-data">
+        <div class="mb-2">
+          <input type="text" name="nome" class="form-control" placeholder="Nome da faculdade" required>
+        </div>
+        <div class="mb-2">
+          <input type="text" name="cnpj" class="form-control" placeholder="CNPJ" required>
+        </div>
+        <div class="mb-2">
+          <input type="text" name="cep" class="form-control" placeholder="CEP" required>
+        </div>
+        <div class="mb-2">
+          <input type="text" name="telefone" class="form-control" placeholder="Telefone" required>
+        </div>
+        <div class="mb-2">
+          <input type="text" name="usuario" class="form-control" placeholder="Usuário" required>
+        </div>
+        <div class="mb-2">
+          <input type="email" name="email" class="form-control" placeholder="E-mail" required>
+        </div>
+        <div class="mb-2">
+          <input type="password" name="senha" class="form-control" placeholder="Senha" required>
+        </div>
+        <div class="mb-2">
+          <input type="file" name="documentacao" class="form-control" placeholder="Documentação">
+        </div>
+        <button type="submit" class="mt-2 btn btn-primary">Cadastrar</button>
+      </form>
     </div>
   </div>
 </body>
