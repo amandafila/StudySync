b235bf80345b2078811a3d59b2382518e6f4e242
Carloslgp <carloslgp3585@gmail.com>
2025-04-10 09:07:14 -0300

    adicionando cadastro_aluno e menu_faculdade

---
+++ menu_faculdade/menu_faculdade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/styles/menu_faculdade.css">
+    <title>Menu Faculdade</title>
+</head>
+<body>
+    <header> 
+        <div class="header_div_titulo">
+            <a class="header_titulo" href="#">StudySync</a>
+        </div>
+        <div class="header_div_faculdade">
+            <a class="header_faculdade" href="#">Faculdade</a>
+        </div>
+    </header>
+
+    <div class="div_botoes">
+        <div class="div_primeira_linha">
+            <a href="#"  class="botao_funcao">Criar grupo</a>
+            <a href="#"  class="botao_funcao">Visualizar grupos</a>
+        </div>
+        <div class="div_segunda_linha">
+            <a href="#"  class="botao_funcao">Visualizar Alunos</a>
+            <a href="#"  class="botao_funcao">Adiconar administrador</a>
+        </div>
+    </div>
+
+</body>
+</html>
