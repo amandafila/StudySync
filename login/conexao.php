c4c7ea903312d3eb33b9fe378be137ff8d2ab40f
Carloslgp <carloslgp3585@gmail.com>
2025-04-11 11:11:28 -0300

    Listar alunos e excluir

--- login/conexao.php
+++ login/conexao.php
@@ -6,7 +6,6 @@ $banco = "studysync";
 
 $conexao = new mysqli($servidor, $usuario, $senha, $banco);
 
-// Verificar conexão
 if ($conexao->connect_error) {
     die("Falha na conexão: " . $conexao->connect_error);
 }
--- menu_faculdade/listar_alunos.php
+++ menu_faculdade/listar_alunos.php
@@ -11,5 +11,49 @@
     <header>
         <h1>Tabela de alunos</h1>
     </header>
+<?php
+    require_once("../cadastro_faculdade/conexao.php");
+    
+    function delete_row($id, $conexao){
+        $delete_sql ="DELETE FROM aluno WHERE id_aluno = $id";
+            if ($conexao->query($delete_sql)) {
+                echo"Deletado com sucesso!";
+            } else {
+                echo "Erro ao deletar: " . $conexao->error;
+            }
+        }
+
+    if(isset($_GET['delete_id'])){
+        delete_row($_GET['delete_id'],$conexao);
+    }
+    
+    $sql = "SELECT id_aluno, email, username, nome, cpf FROM aluno";
+    $resultado = $conexao->query($sql);
+    echo"<table border='1' style='width:600px;'>";
+    echo"<tr>
+            <th>#</th>
+            <th>ID</th>
+            <th>Email</th>
+            <th>Username</th>
+            <th>Nome</th>
+            <th>CPF</th>
+        </tr>";
+    if($resultado->num_rows > 0){
+        while($linha = $resultado->fetch_assoc()){
+            echo"<tr>";
+            echo "<td><button><a href='listar_alunos.php?delete_id=".$linha["id_aluno"]."'>Excluir</a></button>";
+            echo "<td>" . $linha["id_aluno"] . "</td>";
+            echo "<td>" . $linha["email"] . "</td>";
+            echo "<td>" . $linha["username"] . "</td>";
+            echo "<td>" . $linha["nome"] . "</td>";
+            echo "<td>" . $linha["cpf"] . "</td>";
+            echo "</tr>";
+        }
+    }else{
+        echo "<tr><td colspan='3'>Nenhum resultado</td></tr>";
+    }
+    echo "</table>";
+    $conexao->close();
+?>
 </body>
 </html>
