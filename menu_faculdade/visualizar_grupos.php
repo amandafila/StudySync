a2c85de1266d5f5ba9105eea54ed8fd6deeaa725
Arthur Kalil <arthurklfig@gmail.com>
2025-04-24 00:49:51 -0300

    atulização grupos

--- menu_faculdade/visualizar_grupos.php
+++ menu_faculdade/visualizar_grupos.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../assets/styles/visualizar_grupos.css" rel="stylesheet">
+    <link href="../assets/styles/vizu_grupo.css" rel="stylesheet">
     <title>Visualização de grupos</title>
 </head>
 <body>
